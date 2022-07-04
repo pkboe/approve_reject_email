03868884a0d7cf390bcf33e28d93b684896a36e7
Pranil Kharche <pranilkharche@gmail.com>
2022-07-04 14:37:42 +0530

    Fixes

--- index.py
+++ index.py
@@ -4,18 +4,25 @@ import requests
 # get request
 JENKINS_AUTH_CODE = '<JENKINS_AUTH_CODE>'
 app = flask.Flask(__name__) 
-@app.route('/')
-def index():
-    return flask.render_template('From Bridge Server')
 @app.get('/')
+def index(): 
+    return flask.render_template_string(''' hello ''')
+@app.route("/approve", methods=["GET"])
 def main():
     # get JOB_ID, JOB_URL and RESPONSE from get request
-    JOB_ID = flask.request.args.get('JOB_ID')
+    BUILD_ID = flask.request.args.get('BUILD_ID')
     JOB_URL = flask.request.args.get('JOB_URL')
-    DECISION  = flask.request.args.get('RESPONSE')
+    DECISION  = flask.request.args.get('DECISION')
 # xhr.open("POST", "https://jenkins.neebal.com/job/Playpen/job/approval-button/25/input/Approval/abort");
 # xhr.setRequestHeader("Authorization", "Basic c2h1YmhhbWdhaWt3YWQ6MTFlOTY5NzcwZWNjZWVlNWU5NWY0ZGE3Njk1Y2MzMGFjNw==");
-    print(JOB_ID, JOB_URL, DECISION)
+    print(BUILD_ID, JOB_URL, DECISION)
+    # send request to jenkins
+    res = flask.make_response('OK',200)
+    res.headers['Content-Type'] = 'text/plain'
+    res.headers.add('Access-Control-Allow-Origin',
+                    dict(flask.request.headers).get("Origin"))
+    return res
+
 
 # RUn APP
 if __name__ == '__main__':
